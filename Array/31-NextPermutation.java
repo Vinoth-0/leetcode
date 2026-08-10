39b7fc28eed37bb6c15ac8bb9b9c2456ab5b94f1
Vinoth-0 <vinoth132005@gmail.com>
2026-08-10 18:17:20 +0530

    Day 2 sum 2

---
+++ Array/31-NextPermutation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void nextPermutation(int[] num) {
+        int i= num.length-2;
+        while(i>=0 && num[i]>= num[i+1])i--;
+        if(i>=0){
+            int j=num.length-1;
+            while(j>=0 && num[j] <= num[i] )j--;
+            swap(num,i,j);
+        }
+        reverse(num,i+1);
+    }
+    public void swap(int []num,int i,int j){
+        int temp = num[i];
+        num[i]=num[j];
+        num[j]=temp;
+    }
+    public void reverse(int [] num , int start){
+        int end=num.length-1;
+        while(start < end){
+            swap(num,start,end);
+                start++;
+                end--;
+        }
+    }
+}
