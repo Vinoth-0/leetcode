7778a59518c2bbe475c6b3ed33b913b26659a953
Vinoth-0 <vinoth132005@gmail.com>
2026-08-15 23:25:52 +0530

    Daily sums

---
+++ Array/50-Power.java
@@ -0,0 +1,9 @@
+// leetcode 50: Pow(x, n)
+// difficulty: medium
+
+class Solution {
+    public double myPow(double x, int n) {
+         double ans = Math.pow(x, n);   
+         return ans;
+    }
+}
