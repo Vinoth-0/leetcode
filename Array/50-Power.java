7f6cc05484676b08c0557f17232d0e810cb34365
Vinoth Kumar A <vinoth132005@gmail.com>
2026-08-16 15:07:48 +0530

    Implement myPow without built-in Math.pow
    Refactor myPow method to implement power calculation without using Math.pow. Added a helper method solve for recursive power calculation.

--- Array/50-Power.java
+++ Array/50-Power.java
@@ -3,7 +3,32 @@
 
 class Solution {
     public double myPow(double x, int n) {
-         double ans = Math.pow(x, n);   
-         return ans;
+//        By using the Built in method--------->
+//        double ans = Math.pow(x, n);   
+//         return ans;
+//        
+//        By Not using the Built in method--------->
+
+    class Solution {
+    public double myPow(double x, long n) {
+        long power=n;
+
+        if(power<0){
+            x=1/x;
+            power=-power;
+        }
+        return solve(x,power);
+    }
+    public double solve(double x,long n){
+        if(n==0){
+            return 1;
+        }
+        double half=solve(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }
+        return x*half*half;
+    }
+}
     }
 }
