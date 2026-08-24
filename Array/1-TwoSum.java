703d1127e18e55704b11068d81f92bb66f397e54
Vinoth-0 <vinoth132005@gmail.com>
2026-08-24 22:20:41 +0530

    Daily Sums

--- Array/1-TwoSum.java
+++ Array/1-TwoSum.java
@@ -8,9 +8,10 @@ class Solution {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
                 return new int[] { map.get(complement), i };
-            }
-            map.put(nums[i], i);
+            }else {
+                map.put(nums[i], i);
+            }    
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return null;
     }
 }
--- Array/18-4Sum.java
+++ Array/18-4Sum.java
@@ -1,37 +1,23 @@
 // Leetcode-18 4Sum
 // Difficulty - Medium
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
         for (int i = 0; i < n - 3; i++) {
-            // Skip duplicates for first number
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+            if (i > 0 && nums[i] == nums[i - 1])continue;
             for (int j = i + 1; j < n - 2; j++) {
-                // Skip duplicates for second number
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
+                if (j > i + 1 && nums[j] == nums[j - 1])continue;
                 int left = j + 1;
                 int right = n - 1;
                 while (left < right) {
-                    long sum = (long) nums[i]
-                            + nums[j]
-                            + nums[left]
-                            + nums[right];
+                    long sum = (long) nums[i]+ nums[j]+ nums[left]+ nums[right];
                     if (sum == target) {
-                        result.add(Arrays.asList(
-                                nums[i],
-                                nums[j],
-                                nums[left],
-                                nums[right]
-                        ));
+                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
                         left++;
                         right--;
-                        // Skip duplicates
                         while (left < right && nums[left] == nums[left - 1]) {
                             left++;
                         }
